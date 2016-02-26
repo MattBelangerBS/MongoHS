b520cdaa2309cc41d3706cce1b6bc5cc63c802ea
Your Name <you@example.com>
2016-02-26 18:02:02 -0500

    filters working and fixed wierd problem with thing not showing

--- app/scripts/example.jsx
+++ app/scripts/example.jsx
@@ -1,26 +1,71 @@
+
 var Cards = React.createClass({
     getInitialState: function(){
-            return {details: []};
+            return {
+                    details: [], 
+                    filter: '',
+                    hero:'all'
+                    };
     },
     handleClick: function(card) {
     	    this.setState({details: card});
+            console.log(card);
+    },
+    handleFilterChange: function(e) {
+    	    this.setState({filter: e.target.value});
+    },
+    handleFilterClick: function(hero) {
+    	    this.setState({hero: hero});
+            console.log(hero);
     },
     render: function(){
-        var that = this;
-        var cardNodes = this.props.data.map(function(card,index){
-                    if(!(card.health=="30") && !(card.collectible==false)){
-                        return(
-                            <li onClick={that.handleClick.bind(null, card)} key={index}> 
-                                {card.name} 
-                            </li>
-                        )
-                    }
-            });
+            var that = this;
+            var heroes = ['all','neutral','druid','mage','rogue','priest','warrior','hunter','paladin','shaman','warlock']               
             return(
                 <div>
+                    <div className='filters'>
+                        <label> Search Cards By Name
+                            <input 
+                                type="text" 
+                                placeholder="Filter"
+                                value={this.state.filter}
+                                onChange={this.handleFilterChange}
+                            />
+                        </label>
+                        <ul>
+                            {heroes.map(function(hero,index){
+                                return(
+                                    <li onClick={that.handleFilterClick.bind(null, hero)} key={index}>
+                                        {hero}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
                     <div className="left">
                         <ul>
-                        {cardNodes}
+                            {this.props.data.map(function(card,index){
+                                var cardClass = 'default';
+                                if(!(card.health=="30") && !(card.collectible==false) && card.category !=='ability'){
+                                    
+                                    if (card.category === 'minion'){
+                                        cardClass = 'minion'
+                                    }else if(card.category==='spell'){
+                                        cardClass = 'spell'
+                                    }
+                                    var string = card.name;
+                                    var search = (that.state.filter).toLowerCase();
+                                    
+                                        if ((string.toLowerCase().indexOf(search) != -1)&&((that.state.hero==='all') || (that.state.hero===card.hero))){
+                                            return (
+                                                <li className={cardClass} onClick={that.handleClick.bind(null, card)} key={index}> 
+                                                    {card.name} 
+                                                </li>
+                                            )
+                                        } 
+                                    
+                                }
+                            })}
                         </ul>
                     </div>
                     <CardDetails data={this.state.details}/>
@@ -32,15 +77,16 @@ var Cards = React.createClass({
 var CardDetails = React.createClass({
     render: function(){
         var name = this.props.data.name;
-         var description = this.props.data.description;
-         var img = this.props.data.image_url;
-            return(
-                <div className="right">
-                    <h2>{name}</h2>
-                    <h4>{description}</h4>
-                    <img className="img_box" src={img}/>
-                </div>
-            )
+        var description = this.props.data.description;
+        var img = this.props.data.image_url;
+        
+        return(
+            <div className="right">
+                <h2>{name}</h2>
+                <h4>{this.props.data.category}</h4>
+                <img className="img_box" src={img}/>
+            </div>
+        )
     }
 });
 
